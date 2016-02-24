60aedc9aed3155369f7a14e37b6cba0995f28a96
Robert Boggs <robertstephenboggs@gmail.com>
2016-02-23 20:31:18 -0800

    Add token check to server routes

--- utils/router.js
+++ utils/router.js
@@ -1,7 +1,12 @@
 var users = require('./../controllers/userController.js');
 var events = require('./../controllers/eventController.js');
+var helpers = require('./helpers.js');
 
 module.exports = function(app, express) {
+  // authentication middleware used to decode token and made available on the request
+  app.use('/api/users/:id/event', helpers.decode);
+  app.use('/api/events', helpers.decode);
+
   //users routes
   //get requests
   app.get('/api/users/:id/event', users.getUserEvents);
@@ -18,4 +23,4 @@ module.exports = function(app, express) {
   app.post('/api/events', events.addEvent);
   app.post('/api/events/users/:id', events.checkInUser);
   app.delete('/api/events/users/:id', events.removeUserEvent);
-}
+};
