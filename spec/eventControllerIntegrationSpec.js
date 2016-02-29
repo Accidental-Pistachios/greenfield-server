b36b2690a63eb40303325ec339b38ecd8e107de9
Robert Boggs <robertstephenboggs@gmail.com>
2016-02-28 20:18:51 -0800

    Add token authentication to tests

--- spec/eventControllerIntegrationSpec.js
+++ spec/eventControllerIntegrationSpec.js
@@ -17,9 +17,9 @@ var clearDB = function (done) {
   });
 };
 
-
 var testUserId;
 var testEventId;
+var token;
 
 describe('Event Integration Tests', function() {
 
@@ -70,20 +70,34 @@ describe('Event Integration Tests', function() {
       ];
 
       Event.create(events, function(){
-        User.create(users, done);
+        User.create(users, function() {
+
+          request(app)
+          .post('/api/users/signin')
+          .send({
+            email: 'j@b.com', 
+            password: 'rockon'
+          })
+          .end(function(err, res) {
+            token = res.body.token;
+            done();
+          });
+
+        });
       });
+
     });
   });
 
-  describe('Event Creation:', function() {
+  describe('Event Creation:', function(done) {
     it('Add Event method creates a new event, and this event exist in creator\'s events array', function(done) {
       //event created by user 'John' for testing purpose
       var testUserId;
-      User.findOne({'firstName':'John'})
+      User.findOne({'email':'j@b.com'})
       .then(function(user){
-        testUserId = user._id;
         request(app)
         .post('/api/events')
+        .set('x-access-token', token)
         .send({
           type: 'tennis',
           location: '19th & Dolores St, San Francisco, CA 94114',
@@ -91,8 +105,7 @@ describe('Event Integration Tests', function() {
           longitude: -122.426036,
           startTime: new Date('December 17, 2020 03:24:00'),
           endTime: new Date('December 17, 2020 05:24:00'),
-          skillLevel: 'Hobby',
-          userId: testUserId
+          skillLevel: 'Hobby'
         })
         .expect(202)
         .end(function(err) {
@@ -110,21 +123,21 @@ describe('Event Integration Tests', function() {
               expect(newEvent.startTime).to.exist;
               expect(newEvent.skillLevel).to.equal('Hobby');
               expect(newEvent.playerCount).to.equal(1);
-              
-              User.findOne({'firstName':'John'})  
+              User.findOne({'email':'j@b.com'})  
               .then(function (user) {
                 expect(user.events[0]).to.equal(newEvent._id.toString());
                 done();
-              })
+              });
             });
           }
         });
-      })
+      });
     });
 
     it('Get event method gets all events', function(done) {
       request(app)
       .get('/api/events')
+      .set('x-access-token', token)
       .expect(200)
       .end(function(err, res) {
         if (err) {
@@ -152,11 +165,12 @@ describe('Event Integration Tests', function() {
     it('Should remove an user from event, and delete the event if nobody is going', function(done) {
       var testEventId;
       var testUserId;
-      User.findOne({'firstName':'John'})
+      User.findOne({'email':'j@b.com'})
       .then(function(user){
         testUserId = user._id;
         request(app)
         .post('/api/events')
+        .set('x-access-token', token)
         .send({
           type: 'tennis',
           location: '19th & Dolores St, San Francisco, CA 94114',
@@ -164,8 +178,7 @@ describe('Event Integration Tests', function() {
           longitude: -122.426036,
           startTime: new Date('December 17, 2020 03:24:00'),
           endTime: new Date('December 17, 2020 05:24:00'),
-          skillLevel: 'Hobby',
-          userId: testUserId
+          skillLevel: 'Hobby'
         })
         .expect(202)
         .end(function(err) {
@@ -178,6 +191,7 @@ describe('Event Integration Tests', function() {
               testEventId = thisEvent._id;
               request(app)
               .delete('/api/events/users/'+testUserId+'/')
+              .set('x-access-token', token)
               .send({
                 eventId : testEventId
               })
@@ -187,32 +201,34 @@ describe('Event Integration Tests', function() {
                   console.error(err);
                   done(err);
                 } else {
-                  User.findOne({'firstName' : 'John'})
+                  User.findOne({'email':'j@b.com'})
                   .then(function(user){
                     expect(user.events.length).to.equal(0);
                     done();
                   });
                 }
-              })  
-            })
+              });  
+            });
           }
         });
-      })
+      });
     });
 
     it('should check in a user', function(done){
       var testEventId;
+      var testUserId;
 
       Event.findOne({type : 'soccer'})
       .then(function(thisEvent){
         testEventId = thisEvent._id;
 
-        User.findOne({'firstName':'Magee'})
+        User.findOne({'email':'j@b.com'})
         .then(function(user){
           testUserId = user._id;
    
           request(app)
           .post('/api/events/users/'+testUserId+'/')
+          .set('x-access-token', token)
           .send({
             eventId : testEventId
           })
@@ -222,7 +238,7 @@ describe('Event Integration Tests', function() {
               console.error(err);
               done(err);
             } else {
-              User.findOne({'firstName' : 'Magee'})
+              User.findOne({'email':'j@b.com'})
               .then(function(user){
                 expect(user.events[0]).to.equal(testEventId.toString());
                 done();
